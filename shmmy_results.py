ee3beaee761e0841ccf4291a83e6ca9ea2293f14
Panagiotis Georgakopoulos <pankgeorg@gmail.com>
2015-03-07 15:09:01 +0200

    refactor code, simplify usage

--- shmmy_results.py
+++ shmmy_results.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
-import os.path
-import sqlite3
-import argparse
+"""Simple python3 module to poll shmmy.ntua.gr for exam results"""
 import sys
 import time
-#pankgeorg@gmail.com
-#results from shmmy.ntua.gr
+from html.parser import HTMLParser
+import urllib.request
+# pankgeorg@gmail.com
+# results from shmmy.ntua.gr
 
-class bcolors:
+URL = 'https://shmmy.ntua.gr/forum/viewtopic.php?f=290&t=19705'
+POSTS = 1
+START = -5
+SLEEP_TIME = 2
+DBFILE = 'conf.db'
+DEBUG = True and False
+
+
+class TermCol:
+    """color class, copied from stackoverflow.com"""
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
     OKGREEN = '\033[92m'
@@ -15,21 +24,9 @@ class bcolors:
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-URL = 'https://shmmy.ntua.gr/forum/viewtopic.php?f=290&t=19705'
-POSTS = 1
-START = -5
-SLEEP_TIME = 2
-DBFILE = 'conf.db'
 
-def initiallizer():
-    if os.path.isfile(DBFILE):
-        conn = sqlite3.connect(DBFILE)
-        curs = conn.cursor()
-        curs.execute('select url, posts, start from config')
-
-
-from html.parser import HTMLParser
 class ShmmyLastN(HTMLParser):
+    """Class that reads replies from shmmy.ntua.gr/forum"""
     def __init__(self):
         HTMLParser.__init__(self)
         self.flag = False
@@ -39,34 +36,34 @@ class ShmmyLastN(HTMLParser):
 
     def handle_starttag(self, tag, attrs):
         self.stack.append(tag)
-
         if "div" == tag:
             try:
-                x,y = attrs[0]
+                attr_type, attr_val = attrs[0]
             except:
                 return
         else:
             return
-
-        if x == "class"  and y == "content" :
+        if attr_type == "class" and attr_val == "content":
             self.stack.append("THIS")
             self.flag = True
 
     def handle_endtag(self, tag):
-        x = self.stack.pop()
-        if x == "THIS":
+        curr = self.stack.pop()
+        if curr == "THIS":
             self.results.append(self.temp)
             self.flag = False
             self.stack.pop()
             self.temp = ""
         return
 
-    def handle_data (self, data):
+    def handle_data(self, data):
         if not self.flag:
             return
         self.temp += " " + data.strip()
 
+
 class ShmmyCountPosts(HTMLParser):
+    """Class that reads Counts from shmmy.ntua.gr/forum"""
     def __init__(self):
         HTMLParser.__init__(self)
         self.flag = False
@@ -76,12 +73,12 @@ class ShmmyCountPosts(HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == "div":
             try:
-                x,y = attrs[0]
+                attr_type, attr_val = attrs[0]
             except:
                 return
         else:
             return
-        if x == "class" and y == "pagination":
+        if attr_type == "class" and attr_val == "pagination":
             self.flag = True
 
     def handle_endtag(self, tag):
@@ -89,91 +86,92 @@ class ShmmyCountPosts(HTMLParser):
         return
 
     def handle_data(self, data):
-
-        if self.found :
+        if self.found:
             return
-
-        if self.flag and "δημοσιεύσεις" in data:
+        if self.flag and "δημοσιεύσεις" in data:  # or whatever is after num
             try:
-                print (data.strip())
+                if DEBUG:
+                    print(data.strip())
                 self.found = True
                 self.count = int(data.strip().split()[0])
             except:
-                print ("Error reading count")
-                pass
+                if DEBUG:
+                    print("Error reading count")
         else:
             return
 
-def mainprog(start=0):
-    import datetime
-    print("Ώρα: " +  str(datetime.datetime.now()))
-    print()
-    req = urllib.request.urlopen( URL )
-    data = str(req.read(),'utf-8')
+
+def eol_by_79(string):
+    """Wraps strings at 79 chars. Good for terminal, bad for anything else"""
+    new_str = ""
+    old_str = string
+    while old_str:
+        new_str += old_str[0:79] + '\n'
+        old_str = old_str[79:]
+    return new_str
+
+
+def get_count():
+    """Read and return how many posts are now in URL"""
+    req = urllib.request.urlopen(URL)
+    data = str(req.read(), 'utf-8')
     parser = ShmmyCountPosts()
-    parser.feed( data )
-    count = parser.count
+    parser.feed(data)
+    return parser.count
 
-    print(bcolors.OKGREEN + "Έχουν δηλωθεί: ", count, "αποτελέσματα.\n")
-    print("Τελευταία 5:\n")
-    url2 = ( URL +"&start=" + str(parser.count -parser.count%20))
-    data = str(urllib.request.urlopen(url2).read(),"utf-8")
 
+def get_result_list(result_count, page=-1):
+    """Given the result count, read the last page of results"""
+    if page < 0:  # assume that the request is for the last page
+        last_page_count = result_count % 20
+        suffix = str(result_count - last_page_count)
+    else:
+        suffix = str(page * 20)
+    url2 = (URL + "&start=" + suffix)
+    data = str(urllib.request.urlopen(url2).read(), 'utf-8')
     parser = ShmmyLastN()
-    parser.feed ( data )
-    for pr in parser.results[-5:]:
-        print(bcolors.HEADER +"\t" + pr.strip() + bcolors.ENDC)
-        print(bcolors.FAIL + "\t________________\n" + bcolors.ENDC)
+    parser.feed(data)
+    return parser.results
 
-    return count
 
-import urllib.request
+def show_results(result_list, how_many=5):
+    """Take a given list and print results"""
+    if how_many < 1:
+        return
+    print(TermCol.FAIL + 79 * "-" + TermCol.ENDC)
+    for res in result_list[-how_many:]:
+        print(TermCol.HEADER + eol_by_79(res.strip()) + TermCol.ENDC)
+        print(TermCol.FAIL + 79 * "-" + TermCol.ENDC)
+
 
 def main():
-    c = 0
-    start = START
+    """Basic user interaction"""
+    nothing = '\rNothing new [poll # {}]'
+    new_res = TermCol.WARNING + '\r{} new results available ' + TermCol.ENDC
+    prompt = 'Poll for more results? [Y/n] '
+    poll = 0
+    prev_count = 0
+    try:
+        how_many = int(input("How many results to show? "))
+    except:
+        how_many = 5
     while True:
-        req = urllib.request.urlopen( URL )
-        data = str(req.read(),'utf-8')
-        parser = ShmmyCountPosts()
-        parser.feed(data)
-        count = parser.count
-
-        if count <= start:
-            print("\rNothing new (check", c, ")",end ="")
-            c +=1
+        count = get_count()
+        if count == prev_count:
+            print(nothing.format(poll), end="")
+            poll += 1
         else:
-            print(bcolors.WARNING +  "\rΝέο αποτέλεσμα διαθέσιμο!" + bcolors.ENDC)
-            reply = input("Wanna see what it is? [Y/n] (no exits) ")
-            if reply in "yesY \n":
-                mainprog(START)
-            if reply in "exitExitno":
+            print(new_res.format(count-prev_count))
+            res_list = get_result_list(count)
+            show_results(res_list, how_many)
+            if DEBUG:
+                print(res_list)
+            reply = input(prompt)
+            if not reply in "" and reply in "nN":  # Accepts n,N but not \n
                 exit(0)
-            start = count
-
+            prev_count = count
         sys.stdout.flush()
         time.sleep(SLEEP_TIME)
 
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Fetch new results and poll"+
-                                     "for more")
-    parser.add_argument('--new', action='store_true',
-                        help='Use if you want to reset everything')
-    parser.add_argument('--url')
-    parser.add_argument('--time', type=float)
-    parser.add_argument('--posts')
-    parser.add_argument('--start')
-
-    args = parser.parse_args()
-    if args.url:
-        URL = args.url
-    if args.time:
-        SLEEP_TIME = args.time
-    if args.posts:
-        POSTS = args.posts
-    if args.start:
-        START = args.start
-    print(START, POSTS, SLEEP_TIME)
-
     main()
